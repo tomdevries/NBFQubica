9d416e73405441d518d8c2cd3730a4ac1cea8143
tomdevries <t.dvries@chello.nl>
2017-09-14 21:03:54 +0200

    [1.0.9.0] missing files

---
+++ NBF.Qubica.Classes/S_Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBF.Qubica.Classes
+{
+    public class S_Contact
+    {
+        public long id { get; set; }
+        public string email { get; set; }
+        public string message { get; set; }
+    }
+}
